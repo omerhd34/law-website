e06db84e36fc84a4085859c19db19f3775f67fa0
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-03-27 00:41:33 +0300

    shadcn komponentleri güncellendi.

--- components/ui/theme-provider.jsx
+++ components/ui/theme-provider.jsx
@@ -1,11 +1,14 @@
-"use client"
-
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-export function ThemeProvider({
- children,
- ...props
-}) {
- return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }) {
+ return (
+  <NextThemesProvider
+   enableSystem={false}
+   {...props}
+  >
+   {children}
+  </NextThemesProvider>
+ )
 }
+

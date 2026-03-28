8e44f3ed153559b24a1ec02b5b2df8423a8da9ec
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-03-28 17:39:31 +0300

    Layout güncellendi

--- app/(routes)/(site)/layout.js
+++ app/(routes)/(site)/layout.js
@@ -2,13 +2,14 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
 export default function SiteLayout({ children }) {
-  return (
-    <>
-      <Header />
-      <main className="flex min-h-0 min-w-0 flex-1 flex-col">
-        <div className="container mx-auto w-full min-w-0 flex-1">{children}</div>
-      </main>
-      <Footer />
-    </>
-  );
+ return (
+  <>
+   <Header />
+   <main className="flex min-h-0 min-w-0 flex-1 flex-col">
+    <div className="container mx-auto w-full min-w-0 flex-1">{children}
+    </div>
+   </main>
+   <Footer />
+  </>
+ );
 }
